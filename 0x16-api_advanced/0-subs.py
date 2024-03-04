40e011ba87662ae61d10803cd57ce1cff0e3dd62
Habeeb <tijanihabeeb12@outlook.com>
2024-03-04 10:00:47 +0100

    Add: 0. How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""It queries the ``Reddit API`` and returns the number of subscriber"""
+
+import requests
+
+
+def number_of_subscriber(subreddit):
+    """Returns total number of subscribers on a given subreddit"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    resp = requests.get(url, headers=headers, allow_redirects=False)
+    if resp.statusCode == 200:
+        data = resp.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
