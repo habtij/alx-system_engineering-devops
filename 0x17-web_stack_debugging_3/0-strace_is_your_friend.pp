496e946226572bea2391edfd381d4da12868b1d5
Habeeb <tijanihabeeb12@outlook.com>
2024-03-26 07:18:05 +0100

    Add: 0. Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Fix bad "phpp" extensions to "php" in Wordpress settings file
 exec {'fix type in filename':
-  command => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
-  provider=> shell,
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  provider => shell,
 }
