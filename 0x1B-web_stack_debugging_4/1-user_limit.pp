1389bc8e39d1a789bede0830c7d1c4bdb66afe92
Habeeb <tijanihabeeb12@outlook.com>
2024-03-26 08:15:19 +0100

    Add: 1. User limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -3,3 +3,8 @@ exec {'change soft limit':
   command  => 'sudo sed -i "s/holberton\ssoft.*/holberton\tsoft\tnofile\t10000/" /etc/security/limits.conf',
   provider => shell,
 }
+
+exec {'chanfe hard limit':
+  command  => 'sudo sed -i "s/holberton\shard.*/holberton\thard\tnofile\t100000/" /etc/security/limits.conf',
+  provider => shell,
+}
