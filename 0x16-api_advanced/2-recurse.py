d1b0d11ef628849144b713f1a9dc55dbf162bab0
Habeeb <tijanihabeeb12@outlook.com>
2024-04-12 11:38:23 +0100

    Update: 2. Recurse it!

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,7 +8,7 @@ import requests
 
 def recurse(subreddit, hot_list=[], after="", count=0):
     """Returns a list of titles of all hot posts on a given subreddit"""
-    url ="https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {"User-Agent": "0x16-api_advanced:project:v1.0.0 (by /u/habeeb)"}
     params = {
         "after": after,
