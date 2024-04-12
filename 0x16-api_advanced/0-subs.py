8d65793601055def008f65e3562fbbd141b2203a
Habeeb <tijanihabeeb12@outlook.com>
2024-04-12 09:29:46 +0100

    Update: 0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Returns total number of subscribers on a given subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "0x16-api_advanced-newthing (by /u/habeeb)"}
+    headers = {"User-Agent": "firefox:0x16-api_advanced-newthing"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         return 0
